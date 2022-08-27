51bfd3013d7f4c14bdde34f8f453bf5e1eed7cf9
반민정 <minjeong19@naver.com>
2022-08-27 17:46:58 +0900

    디자인도 완료><

--- manam/urls.py
+++ manam/urls.py
@@ -6,4 +6,5 @@ app_name = 'manam'
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('intro/', views.intro, name='intro'),
 ]
--- manam/views.py
+++ manam/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'manam/main.html')
+
+def intro(request):
+    return render(request,'manam/intro.html')
