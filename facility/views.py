0f1022e89cbb1dea05221d49cf4111340006a25f
반민정 <minjeong19@naver.com>
2022-08-27 12:22:47 +0900

    시설 필터링 완료

--- facility/views.py
+++ facility/views.py
@@ -54,11 +54,12 @@ def getlocation(request):
 
 def showFacilitylist(request):
         context = {}
+        facility_list = Facility.objects.order_by('name')
 
         # 사이트에서 필터 선택 여부 받아오기
         b = request.GET.get('b', '')
-        f = request.GET.getlist('f')
-        facility_list = Facility.objects.order_by('name')
+        price = request.GET.get('price', '')
+        print(price)
 
         ########## 카테고리 리스트 ###############
         category_list = Category.objects.all()
@@ -67,15 +68,90 @@ def showFacilitylist(request):
         if b:
             facility_list = facility_list.filter(
                 Q(name__icontains=b) |
-                Q(category__icontains=b)
+                Q(cate_1__icontains=b) |
+                Q(cate_2__icontains=b) |
+                Q(dong__icontains=b) |
+                Q(content__icontains=b)
             ).distinct()
 
-        if f:
-            print(f)
-            query = Q()
-            for i in f:
-                query = query | Q(category__icontains=i)
-            facility_list = facility_list.filter(query)
+        if price == '0':
+            facility_list = facility_list
+        elif price == '1':
+            facility_list = facility_list.filter(cate_1= '공공도서관')
+        elif price == '2':
+            facility_list = facility_list.filter(cate_1= '공공체육시설')
+        elif price == '3':
+            facility_list = facility_list.filter(cate_1= '공연장')
+        elif price == '4':
+            facility_list = facility_list.filter(cate_1= '교육기관')
+        elif price == '5':
+            facility_list = facility_list.filter(cate_1= '도서관')
+        elif price == '6':
+            facility_list = facility_list.filter(cate_1= '동주민센터')
+        elif price == '7':
+            facility_list = facility_list.filter(cate_1= '문화시설')
+        elif price == '8':
+            facility_list = facility_list.filter(cate_1= '문화예술회관')
+        elif price == '9':
+            facility_list = facility_list.filter(cate_1= '문화원')
+        elif price == '10':
+            facility_list = facility_list.filter(cate_1= '미술관')
+        elif price == '11':
+            facility_list = facility_list.filter(cate_1= '박물관/기념관')
+        elif price == '12':
+            facility_list = facility_list.filter(cate_1= '아동청소년지원센터')
+        elif price == '13':
+            facility_list = facility_list.filter(cate_1= '작은도서관')
+        elif price == '14':
+            facility_list = facility_list.filter(cate_1= '노인복지시설(경로당)')
+        elif price == '15':
+            facility_list = facility_list.filter(cate_1= '노인여가복지시설')
+        elif price == '16':
+            facility_list = facility_list.filter(cate_1= '노인의료복지시설(노인요양공동생활가정)')
+        elif price == '17':
+            facility_list = facility_list.filter(cate_1= '노인의료복지시설(노인요양시설)')
+        elif price == '18':
+            facility_list = facility_list.filter(cate_1= '노인주거복지시설')
+        elif price == '19':
+            facility_list = facility_list.filter(cate_1= '재가노인복지시설(재가노인지원)')
+        elif price == '20':
+            facility_list = facility_list.filter(cate_1= '재가노인복지시설(주야간보호)')
+        elif price == '21':
+            facility_list = facility_list.filter(cate_1= '방과후교실')
+        elif price == '22':
+            facility_list = facility_list.filter(cate_1= '지역아동센터')
+        elif price == '23':
+            facility_list = facility_list.filter(cate_1= '틈새돌봄기관')
+        elif price == '24':
+            facility_list = facility_list.filter(cate_1= '거주시설')
+        elif price == '25':
+            facility_list = facility_list.filter(cate_1= '공동생활가정')
+        elif price == '26':
+            facility_list = facility_list.filter(cate_1= '단기거주시설')
+        elif price == '27':
+            facility_list = facility_list.filter(cate_1= '복지관1')
+        elif price == '28':
+            facility_list = facility_list.filter(cate_1= '복지관2')
+        elif price == '29':
+            facility_list = facility_list.filter(cate_1= '지역사회재활시설')
+        elif price == '30':
+            facility_list = facility_list.filter(cate_1= '직업재활시설')
+        elif price == '31':
+            facility_list = facility_list.filter(cate_1= '기타')
+        elif price == '32':
+            facility_list = facility_list.filter(cate_2= '교육·문화')
+        elif price == '33':
+            facility_list = facility_list.filter(cate_2= '노인·청소년')
+        elif price == '34':
+            facility_list = facility_list.filter(cate_2= '보육·가족및여성')
+        elif price == '35':
+            facility_list = facility_list.filter(cate_2= '사회복지일반')
+        else:
+            facility_list = facility_list
+
+            # 필터 옵션
+        context['b'] = b
+        context['price'] = price
 
         # 필터링 된 결과 개수
         context['facility_number'] = facility_list.count()
